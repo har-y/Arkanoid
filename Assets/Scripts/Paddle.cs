250b034586e131c440031c86f8118c50cb2421b5
har-y <jakub.harenda@gmail.com>
2020-11-15 00:36:16 +0100

    Add: paddle movement screen size clamp (paddle script)

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    private Vector2 screenHalfSize;
+
     private float _mouseWorldPosition;
     private float _mousePosition;
-
-    private float _clampLeft;
-    private float _clampRight;
-
+    private float _paddleWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        _clampLeft = -6.05f;
-        _clampRight = 6.05f;
+        _paddleWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        screenHalfSize = new Vector2(Camera.main.aspect * Camera.main.orthographicSize - _paddleWidth, Camera.main.orthographicSize);
     }
 
     // Update is called once per frame
@@ -27,7 +26,7 @@ public class Paddle : MonoBehaviour
     private void PaddleMovement()
     {
         _mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-        _mousePosition = Mathf.Clamp(_mouseWorldPosition, _clampLeft, _clampRight);
+        _mousePosition = Mathf.Clamp(_mouseWorldPosition, -screenHalfSize.x, screenHalfSize.x);
         transform.position = new Vector3(_mousePosition, transform.position.y, 0);
     }
 }
