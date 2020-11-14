1ad3d66761884309bed2ed9c8145243857c5dd88
har-y <jakub.harenda@gmail.com>
2020-11-14 19:57:03 +0100

    Add: paddle movement (paddle script)

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    private Vector3 _mouseWorldPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PaddleMovement();
+    }
+
+    private void PaddleMovement()
+    {
+        _mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(_mouseWorldPosition.x, transform.position.y, 0);
     }
 }
