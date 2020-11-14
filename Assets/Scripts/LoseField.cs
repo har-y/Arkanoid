e8eea72e5c1045252e1e6b3ba38f2f93999f498d
har-y <jakub.harenda@gmail.com>
2020-11-14 18:48:05 +0100

    Add: setup lose field (script)

--- Assets/Scripts/LoseField.cs
+++ Assets/Scripts/LoseField.cs
@@ -7,12 +7,17 @@ public class LoseField : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("game over");
     }
 }
