0022fe4cdca0f08931a8cc47cc5c4d1e153dd987
har-y <jakub.harenda@gmail.com>
2020-11-15 22:36:28 +0100

    Add: ball movement with paddle (ball script)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] private Paddle _paddle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(_paddle.transform.position.x, transform.position.y, transform.position.z);
     }
 }
