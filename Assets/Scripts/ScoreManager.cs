3b9da4fc033dbb9b54164cb859a3fe04956007de
har-y <jakub.harenda@gmail.com>
2020-11-23 20:31:31 +0100

    Add: Score Manager - Script

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private int _score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int _blockPoints)
+    {
+        _score = _score + _blockPoints;
+    }
+}
