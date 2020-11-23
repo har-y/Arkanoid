948cfbaa1583f94c98bdd8859a82ab6086adcb06
har-y <jakub.harenda@gmail.com>
2020-11-23 19:49:26 +0100

    Add: Animation Manager - Script

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    private Animator _animator;
+    private Block _block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _block = GetComponent<Block>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            if (_block.GetIndestructible())
+            {
+                _animator.SetTrigger("gold_block");
+            }
+            else
+            {
+                _animator.SetTrigger("silver_block");
+            }
+        }
+    }
+}
