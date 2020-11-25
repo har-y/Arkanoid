2d2f76abfc210635795fc2ede9b724733f9399e6
har-y <jakub.harenda@gmail.com>
2020-11-25 22:26:24 +0100

    Add: dont destroy canvas singleton (script)

---
+++ Assets/Scripts/DontDestroyCanvas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyCanvas : MonoBehaviour
+{
+    public static DontDestroyCanvas _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
