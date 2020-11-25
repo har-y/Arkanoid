a3121306ecdb9b4d43e3bd338dee0ec953321593
har-y <jakub.harenda@gmail.com>
2020-11-25 23:16:54 +0100

    Add: End Game - Scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,14 +6,16 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private bool _menu;
+    [SerializeField] private bool _endGame;
     [SerializeField] private int _currentLevel;
+    [SerializeField] private int _lastLevel;
     [SerializeField] private int _blocks;
-
 
     // Start is called before the first frame update
     void Start()
     {
         _currentLevel = SceneManager.GetActiveScene().buildIndex;
+        _lastLevel = SceneManager.sceneCountInBuildSettings - 1;
     }
 
     // Update is called once per frame
@@ -43,20 +45,23 @@ public class LevelManager : MonoBehaviour
 
     public void GameOver()
     {
-        int lastIndex = SceneManager.sceneCountInBuildSettings - 1;
-        SceneManager.LoadScene(lastIndex);
+        _endGame = true;
+        SceneManager.LoadScene(_lastLevel);
     }
 
     public void LoadNextLevel()
     {
-        SceneManager.LoadScene(_currentLevel + 1);
+        if (_currentLevel != _lastLevel && !_endGame)
+        {
+            SceneManager.LoadScene(_currentLevel + 1);
+        }
     }
 
     private void BlockLoadNextLevel()
     {
         _blocks = GameObject.FindGameObjectsWithTag("Block").Length;
 
-        if (_blocks <= 0 && !_menu)
+        if ((_blocks <= 0) && (!_menu))
         {
             LoadNextLevel();
         }
