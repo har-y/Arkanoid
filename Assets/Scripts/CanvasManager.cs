e2a8849fd70046fb2acb3a2ea8f42e03ff289c9a
har-y <jakub.harenda@gmail.com>
2020-11-28 19:18:49 +0100

    Add: Canvas Manager - Script

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField] private LevelManager _levelManager;
+    [SerializeField] private GameObject _gameoverField;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelManager = GameObject.FindGameObjectWithTag("Level Manager").GetComponent<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsGameOver();
+    }
+
+    private void IsGameOver()
+    {
+        if (LevelManager.GameState.gameOver == _levelManager.CheckState())
+        {
+            _gameoverField.SetActive(true);
+        }
+        else
+        {
+            _gameoverField.SetActive(false);
+        }
+    }
+}
